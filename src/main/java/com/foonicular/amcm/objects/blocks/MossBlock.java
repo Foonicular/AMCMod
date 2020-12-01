3224dda96778763a5a305b43a4134eba7c3422ee
Foonicular <julesisrad@icloud.com>
2020-12-01 10:36:52 -0800

    Fixed Lily Pads Spawning On Block Bug

--- src/main/java/com/foonicular/amcm/objects/blocks/MossBlock.java
+++ src/main/java/com/foonicular/amcm/objects/blocks/MossBlock.java
@@ -1,21 +1,39 @@
 package com.foonicular.amcm.objects.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.GrassBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
-import net.minecraftforge.common.IPlantable;
 
-public class MossBlock extends GrassBlock {
+public class MossBlock extends Block {
 
 	public MossBlock(Properties properties) {
 		super(properties);
 	}
 	
 	@Override
-	public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction facing, IPlantable plantable) {
-		return true;
-	}
+	   public boolean canSustainPlant(BlockState state, IBlockReader world, BlockPos pos, Direction facing, net.minecraftforge.common.IPlantable plantable) {
+	       net.minecraftforge.common.PlantType type = plantable.getPlantType(world, pos.offset(facing));
+
+	       switch (type) {
+	           case Desert: return false;
+	           case Nether: return false;
+	           case Crop:   return false;
+	           case Cave:   return false;
+	           case Plains: return true;
+	           case Water:  return false;
+	           case Beach:
+	               boolean isBeach = this.getBlock() == Blocks.GRASS_BLOCK || net.minecraftforge.common.Tags.Blocks.DIRT.contains(this) || this.getBlock() == Blocks.SAND;
+	               boolean hasWater = (world.getBlockState(pos.east()).getMaterial() == Material.WATER ||
+	                       world.getBlockState(pos.west()).getMaterial() == Material.WATER ||
+	                       world.getBlockState(pos.north()).getMaterial() == Material.WATER ||
+	                       world.getBlockState(pos.south()).getMaterial() == Material.WATER);
+	               return isBeach && hasWater;
+	       }
+	       return false;
+	   }
 	
 }
--- src/main/java/com/foonicular/amcm/world/biomes/MossyBiome.java
+++ src/main/java/com/foonicular/amcm/world/biomes/MossyBiome.java
@@ -1,5 +1,8 @@
 package com.foonicular.amcm.world.biomes;
 
+import com.foonicular.amcm.init.BlockInit;
+
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
@@ -7,34 +10,31 @@ import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.SeaGrassConfig;
 import net.minecraft.world.gen.feature.structure.MineshaftConfig;
 import net.minecraft.world.gen.feature.structure.MineshaftStructure;
+import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.Placement;
 
 public class MossyBiome extends Biome {
-
+	
+	private static final BlockState RUBY_ORE = BlockInit.RUBY_ORE.get().getDefaultState();
+	
 	public MossyBiome(Builder biomeBuilder) {
 		super(biomeBuilder);
 		this.addStructure(Feature.SWAMP_HUT.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
 		this.addStructure(Feature.MINESHAFT.withConfiguration(new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL)));
 		// For entities: 'entitytype, weight, minspawn, maxspawn
-	    this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.SHEEP, 12, 4, 4));
-	    this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.PIG, 10, 4, 4));
-	    this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.CHICKEN, 10, 4, 4));
-	    this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.COW, 8, 4, 4));
 	    this.addSpawn(EntityClassification.CREATURE, new Biome.SpawnListEntry(EntityType.BEE, 8, 4, 4));
-	    this.addSpawn(EntityClassification.AMBIENT, new Biome.SpawnListEntry(EntityType.BAT, 10, 8, 8));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SPIDER, 100, 4, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE, 95, 4, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ZOMBIE_VILLAGER, 5, 1, 1));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SKELETON, 100, 4, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.CREEPER, 100, 4, 4));
-	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SLIME, 100, 4, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.ENDERMAN, 10, 1, 4));
 	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.WITCH, 5, 1, 1));
-	    this.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(EntityType.SLIME, 1, 1, 1));
 	    DefaultBiomeFeatures.addCarvers(this);
 	    DefaultBiomeFeatures.addStructures(this);
 	    DefaultBiomeFeatures.addLakes(this);
@@ -44,11 +44,13 @@ public class MossyBiome extends Biome {
 	    DefaultBiomeFeatures.addSwampClayDisks(this);
 	    DefaultBiomeFeatures.addSwampVegetation(this);
 	    DefaultBiomeFeatures.addMushrooms(this);
+	    DefaultBiomeFeatures.addHugeMushrooms(this);
 	    DefaultBiomeFeatures.addExtraReedsAndPumpkins(this);
 	    DefaultBiomeFeatures.addSprings(this);
 	    this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.SEAGRASS.withConfiguration(new SeaGrassConfig(64, 0.6D)).withPlacement(Placement.TOP_SOLID_HEIGHTMAP.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
 	    DefaultBiomeFeatures.addFossils(this);
 	    DefaultBiomeFeatures.addFreezeTopLayer(this);
+	    this.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, RUBY_ORE, 4)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 0, 0, 32))));
 	}
 	
 }
